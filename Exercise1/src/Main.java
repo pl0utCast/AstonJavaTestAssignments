fdf920e32b9849c8c29dadc27642b680d1311fe6
stavr0098 <eddybor@gmail.com>
2026-08-17 17:28:30 +0300

    Раскидал классы по файлам, отформатировал код, поменял названия переменных и расположение фигурных скобок, убрал комментарии, заменил способ копирования списка для геттера, заменил ArrayList на абстракцию в классе

--- Exercise1/src/Main.java
+++ Exercise1/src/Main.java
@@ -1,65 +1,7 @@
 void main() {
-    List<Integer> SimpleList = new ArrayList<Integer>(Arrays.asList(4, 5, 6));
-    ImmutableClass iClass = new ImmutableClass(SimpleList);
-    iClass.StrIsDigit("132352");
+    List<Integer> simpleList = new ArrayList<Integer>(Arrays.asList(4, 5, 6));
+    ImmutableClass iClass = new ImmutableClass(simpleList);
+    iClass.strIsDigit("132352");
     System.out.println("work after exception handled(if there any)");
-    iClass.EditInnerClassState("New Value");
     System.out.println(iClass.getList());
-}
-
-protected final class ImmutableClass{
-    private ArrayList<Integer> list = new ArrayList<>(Arrays.asList(1, 2, 3));
-    private final String StringField = "final string";
-    private final SimpleClass SimpleClassField = new SimpleClass(StringField);
-
-    public ImmutableClass(List<Integer> list){
-        this.list = new ArrayList<>();
-        for (Integer item: list){
-            this.list.add(item);
-        }
-    }
-
-    public boolean StrIsDigit(String StringField){
-        try
-        {
-            Integer.parseInt(StringField);
-            System.out.println("true");
-            return true;
-        }
-        catch (NumberFormatException e)
-        {
-            System.out.println("false");
-            return false;
-        }
-    }
-    private void EditInnerClassState(String SimpleField){
-        //Доступ к private-члену класса возможен, потому что оба класса являются вложенными для public final class Main,
-        //заданного неявно
-        SimpleClassField.SimpleField = SimpleField;
-        System.out.println("New value is: " + SimpleField);
-    }
-
-    //Возврат неизменяемой копии
-    public List<Integer> getList() {
-        return Collections.unmodifiableList(list);
-    }
-}
-
-public class SimpleClass{
-    private String SimpleField;
-    public SimpleClass(){
-
-    }
-
-    public SimpleClass(String SimpleField){
-        this.SimpleField = SimpleField;
-    }
-
-    public String getSimpleField(){
-        return SimpleField;
-    }
-
-    public void setSimpleField(String SimpleField){
-        this.SimpleField = SimpleField;
-    }
 }
---
+++ Exercise1/src/SimpleClass.java
@@ -0,0 +1,19 @@
+public class SimpleClass {
+    private String simpleField;
+
+    public SimpleClass() {
+
+    }
+
+    public SimpleClass(String simpleField) {
+        this.simpleField = simpleField;
+    }
+
+    public String getSimpleField() {
+        return simpleField;
+    }
+
+    public void setSimpleField(String simpleField) {
+        this.simpleField = simpleField;
+    }
+}
